0162e0775a59d1cf2841c512a746c509da6bf004
Jessica Sena <jessisena@gmail.com>
2018-12-20 12:22:10 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+// @flow 
+
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import styles from "./App.module.css";
+import { Map, LogoICGC } from "icgc-react-components";
+
+class App extends Component {
+
+	render() {
+		return (
+			<div className="App">
+
+				<header className={styles.AppHeader}>
+					<a className={styles.AppLink} href="https://github.com/geostarters/react-project-template" target="_blank" rel="noopener noreferrer" >
+            ICGC react template
+					</a>
+				</header>
+
+				<div className={styles.AppBody}>
+					<div className={styles.containerMap}>
+						<Map showAttribution={false}></Map>
+					</div>
+					<img src={logo} className={styles.AppLogo} alt="logo" />            
+				</div>
+
+				<LogoICGC className={styles.AppLogo}></LogoICGC>        
+
+			</div>
+		);
+	}
+}
+
+export default App;
